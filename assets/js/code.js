7d6a58b79ccb01838ceb413b3ab6713c2d2bc7b3
Greg D <gregorydesmarais@gmail.com>
2019-08-31 10:20:04 -0400

    Initial page structure

---
+++ assets/js/code.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAyUCWDkIbv230Sevef25coBaeihE1Hv88",
+    authDomain: "trainscheduler-1d254.firebaseapp.com",
+    databaseURL: "https://trainscheduler-1d254.firebaseio.com",
+    projectId: "trainscheduler-1d254",
+    storageBucket: "",
+    messagingSenderId: "64949939814",
+    appId: "1:64949939814:web:013436536cf70ad6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
