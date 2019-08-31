10a01b5a6aa8fd97e078e6447fd5e8b0433c8a27
Greg D <gregorydesmarais@gmail.com>
2019-08-31 11:39:26 -0400

    Removed firebase section and created new API key.

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,15 +1,6 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAyUCWDkIbv230Sevef25coBaeihE1Hv88",
-    authDomain: "trainscheduler-1d254.firebaseapp.com",
-    databaseURL: "https://trainscheduler-1d254.firebaseio.com",
-    projectId: "trainscheduler-1d254",
-    storageBucket: "",
-    messagingSenderId: "64949939814",
-    appId: "1:64949939814:web:013436536cf70ad6"
-};
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+var db = firebase.database();
 
 function updateTime() {
     var cTime = moment().format("hh:mm");
@@ -21,5 +12,12 @@ setInterval(updateTime, 1000);
 
 $("#addTrain").click(function(e) {
     e.preventDefault();
+    $("input[type='text']").each(function() {
+        console.log($(this).attr("id"));
+        if ($(this).val() === "") {
+            alert("Empty found, no data submitted");
+            return false;
+        }
+    })
 
 })
